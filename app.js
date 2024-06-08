9925839d097e08a3e801abc923e6bc756575d8db
vamsi-parisa <vamsiparisa40@gmail.com>
2024-06-08 14:57:46 +0530

    started-learning

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World front React js"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
